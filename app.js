34bd358535ee52cdd71600be36e52876756ea6c7
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-07-18 22:55:03 +0600

    animation problem gone with threshold value

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
-        }
-    })
-})
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            entry.target.classList.toggle('show', entry.isIntersecting)
+        })
+    },
+    {
+        threshold: 0.005
+    }
+)
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el))
